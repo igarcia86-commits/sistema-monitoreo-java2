1d1a8f08cdb2b52ab0efbe7b485e3d11ec71a472
alega <igarci86@alumnos.uaq.mx>
2026-09-17 20:41:00 -0600

    prueba de la clase tanque

--- src/main.java
+++ src/main.java
@@ -1,5 +1,15 @@
 class Main {
     public static void main(String[] args) {
 
+        //prueba de la clase "tanque"
+        tanque miTanque = new tanque("T01", 1000.0);
+
+        System.out.println("Estado: " + miTanque.getEstado());
+        System.out.println("Nivel: " + miTanque.getNivel() + " litros");
+        miTanque.llenar(500);
+
+        System.out.println("\ndespues dede llenar:");
+        System.out.println("Estado actual: " + miTanque.getEstado());
+        System.out.println("Nivel actual: " + miTanque.getNivel() + " litros");
     }
 }
