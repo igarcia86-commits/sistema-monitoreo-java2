a0c39fd47e4c26844e53a2329cbfaca58c9ffb21
alem <alemgori@gmail.com>
2026-09-22 08:33:59 -0600

    prueba del sensor

--- src/main.java
+++ src/main.java
@@ -1,5 +1,15 @@
- class main {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Sistema de monitoreo");
+
+        Tanque tanquePrueba = new Tanque("TK-01", 1000.0);
+        tanquePrueba.llenar(650.0);
+
+        SensorNivel sensor = new SensorNivel("SN-01", tanquePrueba);
+
+        sensor.realizarLectura();
+
+        System.out.println("--- PRUEBA DEL SENSOR ---");
+        System.out.println("Lectura actual: " + sensor.getValorMedido() + " Litros");
+        System.out.println("¿Lectura válida? " + sensor.esLecturaValida());
     }
 }
