89176c658dc21570d8b9dbcc3817e5f1fbfba2bd
alem <alemgori@gmail.com>
2026-09-22 23:48:14 -0600

    integrar tanque y sensor

--- src/main.java
+++ src/main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("--- PRUEBA DEL SENSOR (Tu parte) ---");
         Tanque tanquePrueba = new Tanque("TK-01", 1000.0);
         tanquePrueba.llenar(650.0);
 
@@ -11,16 +10,12 @@ public class Main {
         System.out.println("Lectura actual: " + sensor.getValorMedido() + " Litros");
         System.out.println("¿Lectura válida? " + sensor.esLecturaValida());
 
-        System.out.println("\n----------------------------------\n");
-
-        System.out.println("--- PRUEBA DEL TANQUE (Parte de Alejandro) ---");
         Tanque miTanque = new Tanque("T01", 1000.0);
 
         System.out.println("Estado: " + miTanque.getEstado());
         System.out.println("Nivel: " + miTanque.getNivel() + " litros");
         miTanque.llenar(500);
 
-        System.out.println("\nDespués de llenar:");
         System.out.println("Estado actual: " + miTanque.getEstado());
         System.out.println("Nivel actual: " + miTanque.getNivel() + " litros");
     }
